e36b87d7bcea30381dc48260444699c689969637
don2008 <miro.jancula@gmail.com>
2026-09-12 22:25:28 +0200

    Activate unlock updates when app opens

--- stepswidget/src/main/java/sk/krokywidget/PermissionActivity.kt
+++ stepswidget/src/main/java/sk/krokywidget/PermissionActivity.kt
@@ -82,6 +82,7 @@ class PermissionActivity : ComponentActivity() {
         })
         setContentView(layout)
         StepsWidgetProvider.schedule(this)
+        UnlockUpdateService.start(this)
         checkAndRefresh()
     }
 
