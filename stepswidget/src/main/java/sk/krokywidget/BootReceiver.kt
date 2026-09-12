dcd22ee60ae934918081afa823bd289518551ca1
don2008 <miro.jancula@gmail.com>
2026-09-12 23:28:04 +0200

    Restart widget update service after boot

---
+++ stepswidget/src/main/java/sk/krokywidget/BootReceiver.kt
@@ -0,0 +1,15 @@
+package sk.krokywidget
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
+            intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+            UnlockUpdateService.start(context)
+            StepsWidgetProvider.schedule(context)
+        }
+    }
+}
